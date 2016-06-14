d97d124dffd4dce7367fdf70d7353f0a90d6df05
Jonnes <jpaulo.bcc@gmail.com>
2016-06-13 23:41:26 -0300

    comit inicial

---
+++ App/controllers/produtoController.js
@@ -0,0 +1,23 @@
+﻿/*
+Projeto EAD - AngularJS
+Membros do grupo:
+Guilherme dos Santos Beneti - RA: 448850
+João Paulo de Oliveira - RA: 402796
+Vitor Carnello Jatobá - RA: 53930-9
+*/
+
+angular.module("app")
+.controller("produtoController", function ($scope) {
+
+    //Simulação de um produto já buscado
+    $scope.produto = {
+        Id: 1,
+        Name: "Tomate",
+        Category: "Fruta ou Legume? Eis a questão!!",
+        Price: 1,
+        Url: "http://www.folhadoes.com/cdn/imgs/KQKOOYAPOHN2016011816424618012016.jpg"
+    };
+
+    
+    //Busca em um Serviço REST
+})
